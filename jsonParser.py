08e2f2b9da2f35f36cc161562dbe5b19b9fe64c8
igor <igorsoroczynski@gmail.com>
2024-04-14 14:00:20 +0200

    pr test

--- jsonParser.py
+++ jsonParser.py
@@ -1,13 +1,6 @@
-import boto3
-
 
 def main():
-   client  = boto3.client('ec2')
-
-   resp = client.describe_instances()
-   print(type(resp))
-   print(resp['ResponseMetadata'])
-
+    print("Test pullrequest")
 
 if __name__ == "__main__":
     main()
